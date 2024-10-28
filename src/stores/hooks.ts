f14e4a5c78f8f4b756677c91254d65b94e7b8c8a
Alabi Nurudeen Mohammed <nurudain30@gmail.com>
2024-10-28 15:13:32 +0100

    hooks file  updated

--- src/stores/hooks.ts
+++ src/stores/hooks.ts
@@ -9,7 +9,7 @@ export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 export default axios.create({
    // baseURL: 'http://127.0.0.1:8000'
-   baseURL: 'https://fintechapi-98da.onrender.com'
+   baseURL: 'https://fintechapi-98da.onrender.com:80'
     
 });
 
